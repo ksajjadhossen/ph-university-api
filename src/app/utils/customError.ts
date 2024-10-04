df67794c3760ce3933d2b4ac71f4e62644c9a253
iamdevsajjad <sk.sajjad4207@gmail.com>
2024-10-04 23:53:59 +0600

    removed global error handler for  create new

--- src/app/utils/customError.ts
+++ src/app/utils/customError.ts
@@ -1,17 +1,40 @@
-import { NextFunction, Request } from "express";
+import { ErrorRequestHandler } from "express";
+import { ZodError, ZodIssue } from "zod";
 
-const globalErrorhandler = (
-	err: any,
-	req: Request,
-	res: Response,
-	next: NextFunction
-) => {
+const globalErrorhandler: ErrorRequestHandler = (err, request, res, next) => {
 	const statusCode = err.statusCode || 500;
 	const message = err.message || "Something went wrong";
+
+	type TErrorSource = {
+		path: string | number;
+		message: string;
+	};
+
+	let errorSources: TErrorSource = [
+		{
+			path: "",
+			message: "Something went wrong",
+		},
+	];
+
+	const handleZodError = (err: ZodError) => {
+		const errorSources = err.issues.map((issue: ZodIssue) => {
+			path: issue?.path[issue.path.length - 1];
+			message: issue.message;
+		});
+	};
+
+	if (err instanceof ZodError) {
+		const simplifiedError = handleZodError(err);
+		statusCode = simplifiedError?.statusCode;
+		message = simplifiedError?.message;
+		errorSources = simplifiedError?.errorSources;
+	}
 
 	return res.status(statusCode).json({
 		success: false,
 		message,
+		errorSources,
 		error: err,
 	});
 };
