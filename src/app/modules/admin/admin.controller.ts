3ae6bbd3bef620a777a95dfb74d671b8a004b825
iamdevsajjad <sk.sajjad4207@gmail.com>
2024-10-23 16:46:04 +0600

    get All admin route created successfully.

---
+++ src/app/modules/admin/admin.controller.ts
@@ -0,0 +1,19 @@
+import { RequestHandler } from "express";
+import httpStatus from "http-status";
+import catchAsync from "../../utils/catchAsync";
+import sendResponse from "../../utils/sendResponse";
+import { adminServices } from "./admin.service";
+
+const getAllAdmins: RequestHandler = catchAsync(async (req, res) => {
+	const result = await adminServices.getAllAdmins();
+	sendResponse(res, {
+		statusCode: httpStatus.OK,
+		Success: true,
+		message: "Admins found successfully",
+		data: result,
+	});
+});
+
+export const adminController = {
+	getAllAdmins,
+};
--- src/app/modules/admin/admin.route.ts
+++ src/app/modules/admin/admin.route.ts
@@ -1,6 +1,7 @@
 import express from "express";
+import { adminController } from "./admin.controller";
 const router = express.Router();
 
-router.get("");
+router.get("/", adminController.getAllAdmins);
 
 export const adminRouter = router;
