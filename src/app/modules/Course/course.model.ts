4e515d1b151ad6052f21e5c796cdc55e8496c96e
iamdevsajjad <sk.sajjad4207@gmail.com>
2024-10-28 14:25:16 +0600

    course model is created successfully.

---
+++ src/app/modules/Course/course.model.ts
@@ -0,0 +1,22 @@
+import { model, Schema } from "mongoose";
+import { TCourse, TPrerequisiteCourses } from "./course.interface";
+
+const preRequisiteCoursesSchema = new Schema<TPrerequisiteCourses>({
+	courses: {
+		type: Schema.Types.ObjectId,
+	},
+	isDeleted: {
+		type: Boolean,
+		default: false,
+	},
+});
+
+const courseSchema = new Schema<TCourse>({
+	tittle: { type: String, unique: true, trim: true, required: true },
+	prefix: { type: String, trim: true, required: true },
+	code: { type: Number, trim: true, required: true },
+	credits: { type: Number, trim: true, required: true },
+	prerequisiteCourses: [preRequisiteCoursesSchema],
+});
+
+export const Course = model<TCourse>("Course", courseSchema);
