bd105fd4b81c0a7d71aa3bec16c0d6a60584414b
iamdevsajjad <sk.sajjad4207@gmail.com>
2024-10-28 14:15:18 +0600

    course interface created successfully.

---
+++ src/app/modules/Course/course.interface.ts
@@ -0,0 +1,14 @@
+import { Types } from "mongoose";
+
+export type TPrerequisiteCourses = {
+	courses: Types.ObjectId;
+	isDeleted: boolean;
+};
+
+export type TCourse = {
+	tittle: string;
+	prefix: string;
+	code: number;
+	credits: number;
+	prerequisiteCourses: [];
+};
