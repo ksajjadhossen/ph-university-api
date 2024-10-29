e8451425966aee983115eb0e79d8b406f06fe52c
iamdevsajjad <sk.sajjad4207@gmail.com>
2024-10-29 17:10:43 +0600

    create course validation schema created successfully.

---
+++ src/app/modules/Course/course.validation.ts
@@ -0,0 +1,20 @@
+import { z } from "zod";
+
+const createPreRequisiteCoursesSchema = z.object({
+	courses: z.string(),
+	isDeleted: z.boolean().optional(),
+});
+
+const CreateCourseValidationSchema = z.object({
+	body: z.object({
+		tittle: z.string(),
+		prefix: z.string(),
+		course: z.number(),
+		credits: z.number(),
+		preRequisiteCourses: z.array(createPreRequisiteCoursesSchema),
+	}),
+});
+
+export const courseValidation = {
+	createPreRequisiteCoursesSchema,
+};
