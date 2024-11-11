cdc870c54c031b51523167a19670961db7dfef2c
iamdevsajjad <sk.sajjad4207@gmail.com>
2024-11-11 16:39:46 +0600

    all files created and semesterRegistration route, controller and services created.

---
+++ src/app/modules/semesterRegistration/semesterRegistration.controller.ts
@@ -0,0 +1,67 @@
+import httpStatus from "http-status";
+import catchAsync from "../../utils/catchAsync";
+import sendResponse from "../../utils/sendResponse";
+import { semesterRegistrationServices } from "./semesterRegistration.service";
+
+const createSemesterRegistrationIntoDb = catchAsync((req, res) => {
+	const result = semesterRegistrationServices.createSemesterRegistrationIntoDb(
+		req.body
+	);
+
+	sendResponse(res, {
+		statusCode: httpStatus.OK,
+		Success: true,
+		message: "student Updated successfully",
+		data: result,
+	});
+});
+
+const getAllSemesterRegistrationIntoDb = catchAsync(async (req, res) => {
+	const result =
+		await semesterRegistrationServices.getAllSemesterRegistrationIntoDb();
+
+	sendResponse(res, {
+		statusCode: httpStatus.OK,
+		Success: true,
+		message: "student Updated successfully",
+		data: result,
+	});
+});
+const getSingleSemesterRegistrationIntoDb = catchAsync(async (req, res) => {
+	const SemesterRegistrationId = req.params;
+	const data = req.body;
+	const result =
+		await semesterRegistrationServices.getSingleSemesterRegistrationIntoDb(
+			SemesterRegistrationId
+		);
+
+	sendResponse(res, {
+		statusCode: httpStatus.OK,
+		Success: true,
+		message: "student Updated successfully",
+		data: result,
+	});
+});
+const updateSemesterRegistrationIntoDb = catchAsync(async (req, res) => {
+	const SemesterRegistrationId = req.params;
+	const data = req.body;
+	const result =
+		await semesterRegistrationServices.updateSemesterRegistrationIntoDb(
+			SemesterRegistrationId,
+			data
+		);
+
+	sendResponse(res, {
+		statusCode: httpStatus.OK,
+		Success: true,
+		message: "student Updated successfully",
+		data: result,
+	});
+});
+
+export const semesterRegistrationController = {
+	createSemesterRegistrationIntoDb,
+	getAllSemesterRegistrationIntoDb,
+	getSingleSemesterRegistrationIntoDb,
+	updateSemesterRegistrationIntoDb,
+};
---
+++ src/app/modules/semesterRegistration/semesterRegistration.service.ts
@@ -0,0 +1,34 @@
+import { TSemesterRegistration } from "./semesterRegistration.interface";
+
+const createSemesterRegistrationIntoDb = async (
+	payload: TSemesterRegistration
+) => {
+	const result = 34343;
+
+	return result;
+};
+const getAllSemesterRegistrationIntoDb = async () => {
+	const result = 34343;
+
+	return result;
+};
+const getSingleSemesterRegistrationIntoDb = async (id: string) => {
+	const result = 34343;
+
+	return result;
+};
+const updateSemesterRegistrationIntoDb = async (
+	id: string,
+	payload: TSemesterRegistration
+) => {
+	const result = 34343;
+
+	return result;
+};
+
+export const semesterRegistrationServices = {
+	createSemesterRegistrationIntoDb,
+	getAllSemesterRegistrationIntoDb,
+	getSingleSemesterRegistrationIntoDb,
+	updateSemesterRegistrationIntoDb,
+};
