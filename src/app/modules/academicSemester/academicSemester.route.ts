c20518a790146f11b441026fd832798a5d85b135
iamdevsajjad <sk.sajjad4207@gmail.com>
2024-12-18 16:40:05 +0600

    academic semester validate successfully.

--- src/app/modules/academicSemester/academicSemester.route.ts
+++ src/app/modules/academicSemester/academicSemester.route.ts
@@ -1,21 +1,33 @@
 import express from "express";
+import Auth from "../../middlewares/auth";
 import validateRequest from "../../middlewares/validateRequest";
+import { USER_ROLE } from "../auth/auth.constant";
 import { academicSemesterController } from "./academicSemester.controller";
 import { academicSemesterValidation } from "./academicSemester.validation";
 const router = express.Router();
 
 router.post(
 	"/create-academic-semester",
+	Auth(USER_ROLE.admin),
 	validateRequest(
 		academicSemesterValidation.createAcademicSemesterValidationSchema
 	),
 	academicSemesterController.createAcademicSemester
 );
 
-router.get("/", academicSemesterController.getAllAcademicSemester);
-router.get("/:semesterId", academicSemesterController.findSingleSemesterById);
+router.get(
+	"/",
+	Auth(USER_ROLE.admin, USER_ROLE.faculty, USER_ROLE.student),
+	academicSemesterController.getAllAcademicSemester
+);
+router.get(
+	"/:semesterId",
+	Auth(USER_ROLE.admin, USER_ROLE.faculty, USER_ROLE.student),
+	academicSemesterController.findSingleSemesterById
+);
 router.patch(
 	"/:semesterId",
+	Auth(USER_ROLE.admin),
 	validateRequest(
 		academicSemesterValidation.createAcademicSemesterValidationSchema
 	),
