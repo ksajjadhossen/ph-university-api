be97435cc9b3dc62b8770e71cc115a99708c8cf7
iamdevsajjad <sk.sajjad4207@gmail.com>
2024-12-18 16:32:36 +0600

    course validation created successfully.

--- src/app/modules/Course/course.route.ts
+++ src/app/modules/Course/course.route.ts
@@ -1,5 +1,7 @@
 import express from "express";
+import Auth from "../../middlewares/auth";
 import validateRequest from "../../middlewares/validateRequest";
+import { USER_ROLE } from "../auth/auth.constant";
 import { courseController } from "./course.controller";
 import { courseValidation } from "./course.validation";
 const router = express.Router();
@@ -7,22 +9,30 @@ const router = express.Router();
 router.get("/", courseController.findCoursesFromDb);
 router.post(
 	"/create-course",
+	Auth(USER_ROLE.admin),
 	validateRequest(courseValidation.CreateCourseValidationSchema),
 	courseController.createCourseIntoDb
 );
-router.get("/:id", courseController.findSingleCourseFromDb);
+router.get(
+	"/:id",
+	Auth(USER_ROLE.admin, USER_ROLE.faculty, USER_ROLE.student),
+	courseController.findSingleCourseFromDb
+);
 router.patch(
 	"/:id",
 	courseController.updateCourseToDb,
+	Auth(USER_ROLE.admin),
 	validateRequest(courseValidation.updateCourseValidationSchema)
 );
 router.put(
 	"/:courseId/assign-faculties",
+	Auth(USER_ROLE.admin),
 	validateRequest(courseValidation.facultiesWithCourseValidationSchema),
 	courseController.assignFacultiesIntoDB
 );
 router.delete(
 	"/:courseId/delete-faculties",
+	Auth(USER_ROLE.admin),
 	validateRequest(courseValidation.facultiesWithCourseValidationSchema),
 	courseController.deleteFacultiesFromDB
 );
