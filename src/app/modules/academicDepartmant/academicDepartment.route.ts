ffa2b34a1dfd0193a2cb66cb789838bfc5ddcedf
iamdevsajjad <sk.sajjad4207@gmail.com>
2024-12-18 16:37:32 +0600

    academic department validate successfully.

--- src/app/modules/academicDepartmant/academicDepartment.route.ts
+++ src/app/modules/academicDepartmant/academicDepartment.route.ts
@@ -1,11 +1,14 @@
 import express from "express";
+import Auth from "../../middlewares/auth";
 import validateRequest from "../../middlewares/validateRequest";
+import { USER_ROLE } from "../auth/auth.constant";
 import { academicDepartmentController } from "./academicDepartment.controller";
 import { academicDepartmentValidation } from "./academicDepartment.validation";
 const router = express.Router();
 
 router.post(
 	"/create-academic-department",
+	Auth(USER_ROLE.admin),
 	validateRequest(
 		academicDepartmentValidation.createAcademicDepartmentValidationSchema
 	),
@@ -13,14 +16,17 @@ router.post(
 );
 router.get(
 	"/allAcademicDepartment",
+	Auth(USER_ROLE.admin, USER_ROLE.faculty, USER_ROLE.student),
 	academicDepartmentController.findAllAcademicDepartment
 );
 router.get(
 	"/:AcademicDepartmentId",
+	Auth(USER_ROLE.admin, USER_ROLE.faculty, USER_ROLE.student),
 	academicDepartmentController.findSingleAcademicDepartmentById
 );
 router.patch(
 	"/:AcademicDepartmentId",
+	Auth(USER_ROLE.admin),
 	validateRequest(
 		academicDepartmentValidation.updateAcademicDepartmentValidationSchema
 	),
