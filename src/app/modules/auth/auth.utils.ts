45774868b8e474d7ec70139e7b645780e293aad9
iamdevsajjad <sk.sajjad4207@gmail.com>
2024-12-18 12:33:23 +0600

    create token function created on auth utils.

--- src/app/modules/auth/auth.utils.ts
+++ src/app/modules/auth/auth.utils.ts
@@ -1,12 +1,11 @@
 import jwt from "jsonwebtoken";
-import config from "../../config";
 
-const crateToken = (
+export const crateToken = (
 	JwtPayload: { userId: string; role: string },
 	secret: string,
 	expiresIn: string
 ) => {
-	jwt.sign(JwtPayload, config.jwt_secret_token as string, {
-		expiresIn: "10d",
+	jwt.sign(JwtPayload, secret, {
+		expiresIn,
 	});
 };
